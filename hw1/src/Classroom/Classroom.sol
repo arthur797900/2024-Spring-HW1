475f48684e8be803cf5887056b6493452b3cd0bf
Po Hsun <arthur40602@gmail.com>
2024-03-26 21:42:36 +0800

    fourth

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -3,12 +3,17 @@ pragma solidity ^0.8.0;
 
 /* Problem 1 Interface & Contract */
 contract StudentV1 {
-    // Note: You can declare some state variable
+    uint256 public totalRegistered;
+    mapping(address => bool) public isRegistered;
 
     function register() external returns (uint256) {
-        // TODO: please add your implementaiton here
+        require(!isRegistered[msg.sender], "Student is already registered.");
+        isRegistered[msg.sender] = true;
+        totalRegistered += 1;
+        return totalRegistered;
     }
 }
+
 
 /* Problem 2 Interface & Contract */
 interface IClassroomV2 {

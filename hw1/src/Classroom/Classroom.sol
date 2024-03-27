17f1f210c73c903f8fdfa5d8e8ef0190276338ea
Po Hsun <arthur40602@gmail.com>
2024-03-27 13:48:13 +0800

    fiveth

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -3,17 +3,17 @@ pragma solidity ^0.8.0;
 
 /* Problem 1 Interface & Contract */
 contract StudentV1 {
-    uint256 public totalRegistered;
-    mapping(address => bool) public isRegistered;
-
+    bool private _init = true;
+    
     function register() external returns (uint256) {
-        require(!isRegistered[msg.sender], "Student is already registered.");
-        isRegistered[msg.sender] = true;
-        totalRegistered += 1;
-        return totalRegistered;
+        if (_init){
+            _init = false;
+            return 1000;
+        } else {
+            return 123;
+        }
     }
 }
-
 
 /* Problem 2 Interface & Contract */
 interface IClassroomV2 {
@@ -21,14 +21,28 @@ interface IClassroomV2 {
 }
 
 contract StudentV2 {
+    // 如果 `isEnrolled` 不修改状态，我们可以保持这个函数是 `view` 的。
+    // 否则，我们需要移除 `view`。
     function register() external view returns (uint256) {
-        // TODO: please add your implementaiton here
+        IClassroomV2 classroom = IClassroomV2(msg.sender);
+
+        if (classroom.isEnrolled()) {
+            return 123;
+        } else {
+            return 1000;
+        }
     }
 }
 
 /* Problem 3 Interface & Contract */
 contract StudentV3 {
+    // 这里假设 `register` 函数会因为某种原因消耗 gas，
+    // 尽管它被标记为 `view`。
     function register() external view returns (uint256) {
-        // TODO: please add your implementaiton here
+        if (gasleft() > 7000){
+            return 1000;
+        } else {
+            return 123;
+        }
     }
 }
